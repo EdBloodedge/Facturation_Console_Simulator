356f34f5c79aa25afb26a1ec89614972f1c74605
EdBloodedge <edbloodedge@gmail.com>
2021-06-14 21:16:01 -0500

    Pequeña animación xd
    (/^o^)/

---
+++ Test.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+int main(){
+
+  int i;
+  for(i=0; i<=10; i++){
+    system("cls");
+    printf("(/^o^)/");
+    sleep(1);
+    system("cls");
+    printf("\(^o^\)");
+    sleep(1);
+	}
+
+
+	return 0;
+
+}
