66fffd11f92cf660fd6f9ccad7ad6543f2064b3f
EdBloodedge <edbloodedge@gmail.com>
2021-06-20 16:25:25 -0500

    update

--- Test.c
+++ Test.c
@@ -23,40 +23,42 @@ struct producto{
 
   int clave;
   float precio;
-  char descripcion[40];
+  char descripcion[50];
 
 }PRODUCTOS[25] = {
 //PRODUCTOS
 
-{45121503, 247.70, "Cámaras desechables"},
-{45121504, 6999.00, "Cámaras digitales"},
-{45121505, 13999.00, "Cámaras cinematográficas"},
-{45121506, 10569.00, "Cámaras de video conferencia"},
-{451215010, 24999.00, "Cámaras aéreas"},
-{45121511, 10799.00, "Cámaras de alta velocidad"},
-{45121520, 1209.00, "Cámaras web"},
-{45121521, 354.00, "Cámaras de inspección"},
-{45121522, 950.17, "Cámaras infrarrojas"},
-{45121523, 3076.00, "Cámaras astronómicas"},
-{45121601, 1249.00, "Flashes o iluminación para cámaras"},
-{45121602, 799.00, "Tripodes para cámara"},
-{45121603, 63.85, "Lentes para cámara"},
-{45121604, 267.24, "Oclusores para cámaras"},
+{45121503, 247.70, "Camaras desechables"},
+{45121504, 6999.00, "Camaras digitales"},
+{45121505, 13999.00, "Camaras cinematograficas"},
+{45121506, 10569.00, "Camaras de video conferencia"},
+{451215010, 24999.00, "Camaras aereas"},
+{45121511, 10799.00, "Camaras de alta velocidad"},
+{45121520, 1209.00, "Camaras web"},
+{45121521, 354.00, "Camaras de inspeccion"},
+{45121522, 950.17, "Camaras infrarrojas"},
+{45121523, 3076.00, "Camaras astronomicas"},
+{45121601, 1249.00, "Flashes o iluminacion para camaras"},
+{45121602, 799.00, "Tripodes para camara"},
+{45121603, 63.85, "Lentes para camara"},
+{45121604, 267.24, "Oclusores para camaras"},
 {45121605, 7400.00, "Marcos de pantalla"},
 
 //SERVICIOS
 
-{82131601, 6500.00, "Servivios de fotografía aérea"},
-{82131602, 6315.00, "Cinematografía"},
-{82131603, 4950.00, "Servicios de producción de vídeos"},
-{82131604, 3500.00, "Servicios de estudio fotográfico o fotos fijas"},
-{82131501, 7500.00, "Elaboración o reproducción de película fija"},
-{82131502, 6315.00, "Elaboración o reproducción de películas de cine"},
+{82131601, 6500.00, "Servivios de fotografia aerea"},
+{82131602, 6315.00, "Cinematografia"},
+{82131603, 4950.00, "Servicios de produccion de videos"},
+{82131604, 3500.00, "Servicios de estudio fotografico o fotos fijas"},
+{82131501, 7500.00, "Elaboracion o reproduccion de pelicula fija"},
+{82131502, 6315.00, "Elaboracion o reproduccion de peliculas de cine"},
 {82131503, 2572.90, "Servicios de microficha"},
-{82131504, 175.90, "Separación de colores"},
-{82131505, 3750.00, "Servicio de postproducción de películas"},
-{82131506, 2950.00, "Servicio de fotos de graduación"}
+{82131504, 175.90, "Separacion de colores"},
+{82131505, 3750.00, "Servicio de postproduccion de peliculas"},
+{82131506, 2950.00, "Servicio de fotos de graduacion"}
 };
+
+
 
 int modificar(struct estudiante *a){
 
